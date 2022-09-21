766d1e38e2b02678a21ade6568456f3cd7c99676
Detty434 <detty12323@yahoo.com>
2022-09-20 20:20:06 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+* void prototype()
+* int prototype()
+*/
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int_strlen(char c);
+void _puts(char *str);
+void print_rev(char *s);
+void puts2 (char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_sterpy(char *dest, char  *src);
+int _atoi(char *str);
+int _putchar(char c)
+
+#endif /*MAIN_H*/
